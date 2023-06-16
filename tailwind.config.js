bc132aef2ce3fd8198f07a5190cfb4cfe5c03c78
fajareka1 <fajar.ekasandiyuda@gmail.com>
2023-06-16 09:16:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  darkMode: 'class',
+  theme: {
+    container:{
+      center:true,
+      padding: '16px',
+    },
+    extend: {
+      screens : {
+        '2xl' : '1320px',
+      },
+      colors:{
+        secondary : '#64748b'
+      }
+    },
+  },
+  plugins: [],
+}
+
